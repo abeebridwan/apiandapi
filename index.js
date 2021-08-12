51577ce7c8e6206dbaebb4f78716d4c29a8c9ac0
Abeeb Ridwan Olumide <igortunde@yahoo.com>
2021-08-12 11:51:16 +0100

    server running

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ const express = require('express');
 const axios = require('axios');
 
 const app = express();
+PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello there')
+app.get('/', (_, res) => {
+  res.send('how are you !Hola')
 })
+
+app.listen(PORT, () => console.log(`Server Ready on ${PORT}`))
